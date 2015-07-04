8a128c0cf0352cf2d01010181c9e154ddadea2b3
warlyware <danward@gmail.com>
2015-07-03 21:44:02 -0700

    First commit

---
+++ public/javascripts/tests.js
@@ -0,0 +1,108 @@
+var request = superagent;
+
+describe('Our API', function(){
+  describe('POST empty data to /questions', function(){
+    it("returns a 400", function(done) {
+      request.post("/questions").end(function(err, res) {
+        if (err) { throw err; }
+        expect(res.status).to.equal(400);
+        done();
+      });
+    });
+  });
+  describe('POST valid data to /questions', function(){
+    it("returns the saved question in json", function(done) {
+      request.post("/questions")
+        .send({body: 'one test question', email: 'test@test.com'})
+        .end(function(err, res) {
+        if (err) { throw err; }
+        expect(res.status).to.equal(200);
+        expect(res.body.email).to.equal('test@test.com');
+        expect(res.body.slug).to.equal('one-test-question');
+        done();
+      });
+    });
+  });
+  describe('POST duplicate data to /questions', function(){
+    it("returns a 400", function(done) {
+      request.post("/questions")
+        .send({body: 'one test question', email: 'test@test.com'})
+        .end(function(err, res) {
+        if (err) { throw err; }
+        expect(res.status).to.equal(400);
+        done();
+      });
+    });
+  });
+  describe('GET /questions', function(){
+    it("returns an array of all the questions", function(done) {
+      request.get("/questions").end(function(err, res) {
+        if (err) { throw err; }
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body[0].body).to.equal("one test question");
+        done();
+      });
+    });
+  });
+  describe('GET /questions/:questionCode', function(){
+    it("returns the question which have that code", function(done) {
+      request.get("/questions/one-test-question").end(function(err, res) {
+        if (err) { throw err; }
+        expect(res.status).to.equal(200);
+        expect(res.body.body).to.equal("one test question");
+        done();
+      });
+    });
+  });
+  describe('GET /questions/:questionCode', function(){
+    it("returns 404 if the code does not exist", function(done) {
+      request.get("/questions/dsajflkasdjfjadsjflkjaslkdf").end(function(err, res) {
+        if (err) { throw err; }
+        expect(res.status).to.equal(404);
+        done();
+      });
+    });
+  });
+  describe('PATCH /questions/:questionCode', function(){
+    it("returns 404 if the code does not exist", function(done) {
+      request.patch("/questions/dsajflkasdjfjadsjflkjaslkdf").end(function(err, res) {
+        if (err) { throw err; }
+        expect(res.status).to.equal(404);
+        done();
+      });
+    });
+  });
+  describe('PATCH /questions/:questionCode', function(){
+    it("update the question with new data", function(done) {
+      request.patch("/questions/one-test-question")
+      .send({body: 'it is new now', email: 'hacker@test.com'})
+      .end(function(err, res) {
+        if (err) { throw err; }
+        expect(res.status).to.equal(200);
+        expect(res.body.body).to.equal("it is new now");
+        expect(res.body.email).to.equal("hacker@test.com");
+        done();
+      });
+    });
+  });
+  describe('DELETE /questions/:questionCode', function(){
+    it("returns 404 if the code does not exist", function(done) {
+      request.del("/questions/dsajflkasdjfjadsjflkjaslkdf").end(function(err, res) {
+        if (err) { throw err; }
+        expect(res.status).to.equal(404);
+        done();
+      });
+    });
+  });
+  describe('DELETE /questions/:questionCode', function(){
+    it("update the question with new data", function(done) {
+      request.del("/questions/one-test-question")
+      .end(function(err, res) {
+        if (err) { throw err; }
+        expect(res.status).to.equal(200);
+        done();
+      });
+    });
+  });
+});
---
+++ routes/index.js
@@ -0,0 +1,124 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var MD5 = require("MD5");
+var slug = require("slug");
+
+mongoose.connect(process.env.MONGOLAB_URI);
+
+// var User = mongoose.model("users", { name: String });
+// var user1 = new User({name: 'from Mongoos'});
+// user1.save();
+//
+// User.find({}, function(err, data) {
+//   console.log(data);
+// });
+
+var Question = mongoose.model("Question", {
+  slug: { type: String, required: true, unique: true },
+  body: { type: String, required: true, unique: true },
+  email: { type: String, required: true },
+  gravatarUrl: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+// temp500Qs = Array.apply(null, Array(500)).map(function(n, i) { return { body: "Q" + i, email: 'test@test.com', slug: "q-" + i, gravatarUrl: "test" } });
+// Question.create(temp500Qs, function(err) {
+//   console.error(err);
+// });
+
+Question.on('index', function(err) {
+  if (err) {
+    console.error(err);
+  }
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mongo API' });
+});
+
+router.get('/tests', function(req, res, next) {
+  Question.remove({}, function(err) {
+    res.render('tests');
+  });
+});
+
+router.get('/test', function(req, res, next) {
+  res.send("just testing");
+});
+
+router.post('/test', function(req, res, next) {
+  var response = req.body;
+  response.serverTime = new Date();
+  res.json(response);
+});
+
+router.post("/questions", function(req, res) {
+  var question = new Question(req.body);
+
+  question.slug = slug(req.body.body || '');
+  question.gravatarUrl = "http://www.gravatar.com/avatar/" + MD5(req.body.email);
+
+  question.save(function(err, savedQuestion) {
+    if (err) {
+      console.log(err);
+      res.status(400).json({ error: "Validation Failed" });
+    }
+    console.log("savedQuestion:", savedQuestion);
+    res.json(savedQuestion);
+  });
+});
+
+router.get("/questions", function(req, res) {
+  console.log('qwer');
+  Question.find({}).sort({ createdAt: 'desc' }).limit(3).exec(function(err, questions) {
+    if (err) {
+      console.log(err);
+      res.status(400).json({ error: "Could not read questions data" });
+    }
+    res.json(questions);
+  });
+});
+
+router.get("/questions/:questionCode", function(req, res) {
+  Question.findOne({slug: req.params.questionCode}).exec(function(err, question) {
+    if (err) {
+      console.log(err);
+      res.status(400).json({ error: "Could not read questions data" });
+    }
+    if (!question) {
+      res.status(404);
+    }
+    res.json(question);
+  });
+});
+
+router.patch("/questions/:questionCode", function(req, res) {
+  Question.findOneAndUpdate({ slug: req.params.questionCode }, req.body, { new: true }, function(err, updatedQuestion) {
+    console.log(err, updatedQuestion);
+    if (err) {
+      console.log(err);
+      res.status(400).json({ error: "Could not read questions data" });
+    }
+    if (!updatedQuestion) {
+      res.status(404);
+    }
+    res.json(updatedQuestion);
+  });
+});
+
+router.delete("/questions/:questionCode", function(req, res) {
+  Question.findOneAndRemove({ slug: req.params.questionCode }, function(err, updatedQuestion) {
+    console.log(err, updatedQuestion);
+    if (err) {
+      console.log(err);
+      res.status(400).json({ error: "Could not read questions data" });
+    }
+    if (!updatedQuestion) {
+      res.status(404);
+    }
+    res.json({message: 'question deleted'});
+  });
+});
+
+module.exports = router;
